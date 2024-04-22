97d3e0c95604945dc9aeab727348e20c87a54d9d
asteriamelisa <145691342+asteriamelisa@users.noreply.github.com>
2024-04-22 21:29:30 +0700

    edit2an

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,11 +13,121 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'adminuser@email.com',
+            'type' => 1,
+            'password' => bcrypt('123456')
+        ]);
+
+        User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@email.com',
+            'type' => 2,
+            'password' => bcrypt('123456')
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@email.com',
+            'type' => 0,
+            'password' => bcrypt('123456')
+        ]);
+
+
+        User::create([
+            'name' => 'Superadmin',
+            'email' => 'superadmin@email.com',
+            'type' => 3,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'type' => 1,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Admin Akademik',
+            'email' => 'akademi@email.com',
+            'type' => 1,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Admin Keuangan',
+            'email' => 'keuangan@email.com',
+            'type' => 1,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Admin LPPM',
+            'email' => 'lppm@email.com',
+            'type' => 1,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Admin SDM',
+            'email' => 'sdm@email.com',
+            'type' => 1,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Direktur',
+            'email' => 'direktur@email.com',
+            'type' => 2,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Wakil Direktur 1',
+            'email' => 'wd1@email.com',
+            'type' => 2,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Wakil Direktur 2',
+            'email' => 'wd2@email.com',
+            'type' => 2,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Wakil Direktur 3',
+            'email' => 'wd3@email.com',
+            'type' => 2,
+            'password' => bcrypt('12345')
+        ]);
+
+         // User tipe 0-User  
+
+        User::create([
+            'name' => 'Dosen',
+            'email' => 'dosen@email.com',
+            'type' => 0,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Mahasiswa',
+            'email' => 'mahasiswa@email.com',
+            'type' => 0,
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Tendik',
+            'email' => 'tendrik@email.com',
+            'type' => 0,
+            'password' => bcrypt('12345')
+        ]);
+
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
-        }),
+        })
     ],
+  server: {
+    https: true,
+    hmr: {
+        host: 'localhost',
+    },
+  }
 });
